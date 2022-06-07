d801e6d20c4ebe9c8a481e5a2ab8322909809985
slitefull <viptitechko1@gmail.com>
2022-06-07 23:59:45 +0300

    feat: added content

--- server/app.js
+++ server/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const schema = require('../schema/schema');
 const { graphqlHTTP } = require('express-graphql');
+const Cors = require('cors');
 
 const app = express();
 const PORT = 3005;
+
+app.use(Cors());
 
 app.use('/graphql', graphqlHTTP({
   schema,
